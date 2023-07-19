964bf822c664228c15e0820c679328b4f0fe3f5d
JulioCesarCandidoMarquezin <juliocesarmarquezin@gmail.com>
2023-07-19 10:18:20 -0400

    Retirado o "require products"

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,7 @@
-const { productsRoutes } = require("./products");
 const { postsRoutes } = require("./posts");
 const { usersRoutes } = require("./users");
 
 module.exports = (fastify) => {
     usersRoutes(fastify),
-    postsRoutes(fastify),
-    productsRoutes(fastify)
+    postsRoutes(fastify)
 }
