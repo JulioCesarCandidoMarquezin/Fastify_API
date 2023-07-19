505d1d73db73c0c1eb6d15834268ec63247f1f07
JulioCesarCandidoMarquezin <juliocesarmarquezin@gmail.com>
2023-07-19 09:53:44 -0400

    Criados os users e posts

---
+++ src/routes/posts.js
@@ -0,0 +1,44 @@
+exports.postsRoutes = (fastify) => {
+  fastify.get("/posts", function (resquest, reply) {
+    fastify.mysql.query("SELECT * FROM posts", function (error, results) {
+      reply.send(error || results);
+    });
+  });
+
+  fastify.get("/posts/:id", function (request, reply) {
+    fastify.mysql.query(
+      `SELECT * FROM posts where posts.id = ${Number(request.params.id)}`,
+      function onResult(error, results) {
+        reply.send(error || results);
+      }
+    );
+  });
+
+  fastify.post("/posts", function (request, reply) {
+    fastify.mysql.query(
+      `INSERT INTO posts (title, body, userId) VALUES ('${request.body.title}', '${request.body.body}', ${Number (request.body.userId)})`,
+      function onResult(error, results) {
+        reply.send(error || results);
+      }
+    );
+  });
+
+  fastify.put("/posts/:id", function (request, reply) {
+    fastify.mysql.query(
+      `UPDATE posts SET title = '${request.body.title}', body = '${
+        request.body.body}' where posts.id = ${Number(request.params.id)}`,
+      function onResult(error, results) {
+        reply.send(error || results);
+      }
+    );
+  });
+
+  fastify.delete("/posts/:id", function (request, reply) {
+    fastify.mysql.query(
+      `DELETE FROM posts WHERE posts.id = ${Number(request.params.id)}`,
+      function onResult(error, results) {
+        reply.send(error || results);
+      }
+    );
+  });
+};
---
+++ src/routes/users.js
@@ -0,0 +1,44 @@
+exports.usersRoutes = (fastify) => {
+    fastify.get("/users", function (resquest, reply) {
+      fastify.mysql.query("SELECT * FROM users", function (error, results) {
+        reply.send(error || results);
+      });
+    });
+  
+    fastify.get("/users/:id", function (request, reply) {
+      fastify.mysql.query(
+        `SELECT * FROM users where users.id = ${Number(request.params.id)}`,
+        function onResult(error, results) {
+          reply.send(error || results);
+        }
+      );
+    });
+  
+    fastify.post("/users", function (request, reply) {
+      fastify.mysql.query(
+        `INSERT INTO users (name, password) VALUES ('${request.body.name}', '${request.body.name}')`,
+        function onResult(error, results) {
+          reply.send(error || results);
+        }
+      );
+    });
+  
+    fastify.put("/users/:id", function (request, reply) {
+      fastify.mysql.query(
+        `UPDATE users SET name = '${request.body.name}' where users.id = ${Number(request.params.id)}`,
+        function onResult(error, results) {
+          reply.send(error || results);
+        }
+      );
+    });
+  
+    fastify.delete("/users/:id", function (request, reply) {
+      fastify.mysql.query(
+        `DELETE FROM users WHERE users.id = ${Number(request.params.id)}`,
+        function onResult(error, results) {
+          reply.send(error || results);
+        }
+      );
+    });
+  };
+  
